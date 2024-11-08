e288f95ace631969eb02febc1a2341e16e94f4f1
dipok <dipoknathdipu@gmail.com>
2024-11-08 16:22:37 +0600

    rotate list

---
+++ Linked_list/rotated_list.cpp
@@ -0,0 +1,210 @@
+#include <iostream>
+
+using namespace std;
+
+class node
+{
+    int data;
+
+public:
+    node *next;
+    node(int ele)
+    {
+        this->data = ele;
+        this->next = NULL;
+    }
+    void getvalue()
+    {
+        cout << this->data;
+    }
+};
+
+// linked list
+class list
+{
+public:
+    node *root;
+    int size;
+
+public:
+    void print()
+    {
+        cout << root << "\n";
+    }
+    list()
+    {
+        this->root = NULL;
+        this->size = 0;
+    }
+    void display();
+    void push_front(int ele);
+    void push_back(int ele);
+    void insert_At(int ele, int k);
+    void pop_back();
+    void pop_front();
+    void reverse_list();
+    // void rotated
+};
+
+void list::pop_front()
+{
+    if (size == 0)
+        return;
+
+    node *temp = root;
+    root = temp->next;
+    delete (temp);
+    size--;
+    return;
+}
+void list::pop_back()
+{
+    if (size == 0)
+        return;
+
+    node *temp = root;
+
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+    node *del = temp->next;
+    temp->next = NULL;
+    delete (del);
+    return;
+}
+void list::display()
+{
+    if (root == NULL)
+        return;
+
+    node *temp = root;
+
+    while (temp != NULL)
+    {
+        temp->getvalue();
+        cout << "-> ";
+        temp = temp->next;
+    }
+}
+void list::push_front(int ele)
+{
+
+    if (this->root == NULL)
+    {
+        size++;
+        node *newnode = new node(ele);
+        this->root = newnode;
+        return;
+    }
+    size++;
+    node *newnode = new node(ele);
+    newnode->next = root;
+    root = newnode;
+    return;
+}
+void list::push_back(int ele)
+{
+
+    if (root == NULL)
+    {
+        size++;
+        node *newnode = new node(ele);
+
+        root = newnode;
+        return;
+    }
+
+    node *newnode = new node(ele);
+
+    node *temp = root;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newnode;
+    size++;
+    return;
+}
+
+void list::reverse_list()
+{
+
+    if (root == NULL)
+        return;
+
+    node *pre = NULL;
+    node *cur = root;
+    node *next = cur->next;
+
+    while (cur != NULL)
+    {
+        node *temp = cur;
+        cur->next = pre;
+        pre = cur;
+        cur = next;
+        if (cur != NULL)
+            next = next->next;
+    }
+    root = pre;
+    // cout <<  << endl;
+}
+
+node *rotated(node *head, int k)
+{
+
+    if (head == NULL)
+        return head;
+
+    // calculate size of list......
+
+    int size = 0;
+    node *temp = head;
+    while (temp)
+    {
+        size++;
+        temp = temp->next;
+    }
+
+    k = k % size;
+    if (k == 0)
+        return head;
+
+    int move = size - k - 1;
+
+    node *cur = head;
+    while (move)
+    {
+        cur = cur->next;
+        move--;
+    }
+    node *newhead = cur->next;
+    cur->next = NULL;
+    temp = newhead;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = head;
+
+    return newhead;
+}
+
+int main()
+{
+    list l;
+    l.push_front(3);
+    l.push_back(8);
+    l.push_back(34);
+    l.push_front(23);
+    l.display();
+    l.reverse_list();
+    cout << endl;
+    l.display();
+    // cout << l.root;
+    l.root = rotated(l.root, 0);
+
+    cout << endl
+         << "ok ";
+    l.display();
+}
